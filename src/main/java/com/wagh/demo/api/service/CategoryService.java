3d4a0bd230673c921ee794e318f363ffc2b28bb2
hi-roshni <roshaniwagh00@gmail.com>
2024-09-02 18:15:52 +0530

    added apis

---
+++ src/main/java/com/wagh/demo/api/service/CategoryService.java
@@ -0,0 +1,44 @@
+package com.wagh.demo.api.service;
+
+import com.wagh.demo.api.model.Category;
+import com.wagh.demo.api.repo.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category category) {
+        if (categoryRepository.existsById(id)) {
+            category.setId(id);
+            return categoryRepository.save(category);
+        }
+        return null;
+    }
+
+    public Boolean deleteCategory(Long id) {
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
